4d5535d9dd36cd5fb532491ddc9359b72b3b238a
tsumli <28624172+tsumli@users.noreply.github.com>
2024-04-23 23:17:43 +0900

    add:

--- src/vlux/common/descriptor_sets.h
+++ src/vlux/common/descriptor_sets.h
@@ -7,7 +7,8 @@ namespace vlux {
 class DescriptorSets {
    public:
     DescriptorSets(const VkDevice device, const VkDescriptorSetAllocateInfo& alloc_info) {
-        if (vkAllocateDescriptorSets(device, &alloc_info, &descriptor_sets_) != VK_SUCCESS) {
+        descriptor_sets_.resize(alloc_info.descriptorSetCount);
+        if (vkAllocateDescriptorSets(device, &alloc_info, descriptor_sets_.data()) != VK_SUCCESS) {
             throw std::runtime_error("failed to allocate descriptor sets!");
         }
     }
@@ -15,10 +16,11 @@ class DescriptorSets {
     DescriptorSets(const DescriptorSets&) = default;
     DescriptorSets& operator=(const DescriptorSets&) = default;
 
-    VkDescriptorSet GetVkDescriptorSet() const { return descriptor_sets_; }
+    VkDescriptorSet GetVkDescriptorSet(const size_t idx) const { return descriptor_sets_.at(idx); }
+    const VkDescriptorSet* GetVkDescriptorSetPtr() const { return descriptor_sets_.data(); }
 
    private:
-    VkDescriptorSet descriptor_sets_;
+    std::vector<VkDescriptorSet> descriptor_sets_;
 };
 }  // namespace vlux
 #endif
