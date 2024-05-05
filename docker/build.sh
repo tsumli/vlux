891d9a535679a0ffaaa54e718def16b536169ed0
tsumli <28624172+tsumli@users.noreply.github.com>
2024-05-05 07:45:56 +0000

    update docker

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,7 @@ NAME="vulkan-rendering"
 docker build \
     -t $NAME:latest \
     --file docker/Dockerfile \
-     --network host \
-     --build-arg USER=$(whoami) \
-     .
+    --network host \
+    --build-arg USER=$(id -un) \
+    --build-arg GROUP=$(id -gn) \
+    .
