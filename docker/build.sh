220f35d2d76b1fcbca748fca75bc6b21ab0d4742
Tomoyuki Hatakeyama <28624172+tsumli@users.noreply.github.com>
2024-05-13 20:28:08 +0900

    add scripts (#72)

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,6 @@ docker build \
     -t $NAME:latest \
     --file docker/Dockerfile \
     --network host \
-    --no-cache \
     --build-arg USER=$(id -un) \
     --build-arg GROUP=$(id -gn) \
     .
